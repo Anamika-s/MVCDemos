ad98fe8133733fc5bb4696df2fd554cee6d821f7
anamikasawhney <anamika.sawhney22@gmail.com>
2021-06-10 11:26:10 +0530

    MVCDemos

---
+++ WebApplication14/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication14.Controllers
+{
+    public class HomeController : Controller
+    {
+
+     List<String> breakingNews = new List<String>()      //DO NOT change this declaration and values
+        {
+            "PM visit brings business","10% slash in GST","Top Player announced retirement","India wins series"
+        };
+
+        public ActionResult NewsByChoice(int id)
+        {
+            return Content(breakingNews[id].ToString());
+        }// Implement 'NewsByChoice' acti
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult AddNumbers1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNumbers1(string txtNo1, string txtNo2)
+        {
+            int res = int.Parse(txtNo1) + int.Parse(txtNo2);
+            ViewBag.res = res;
+            return View();
+        }
+
+    }
+}
---
+++ WebApplication14/Controllers/StudentController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication14.Models;
+
+namespace WebApplication14.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>()
+            {
+                 new Student(){ Id=1, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=2, Name="Deepak", Marks=89, BatchCode="B002"},
+                 new Student(){ Id=3, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=4, Name="Deepak", Marks=89, BatchCode="B002"},
+                 new Student(){ Id=5, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=6, Name="Deepak", Marks=89, BatchCode="B002"},
+                 new Student(){ Id=7, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=8, Name="Deepak", Marks=89, BatchCode="B002"},
+                };
+            ViewBag.students = students;
+            return View();
+              }
+
+        public ActionResult Display()
+        {
+            Student student = new Student() { Id = 1, Name = "Ajay", Marks = 90, BatchCode = "B001" };
+            ViewBag.student = student;
+            return View();
+        }
+
+        public ActionResult Display1()
+        {
+            Student student = new Student() { Id = 1, Name = "Ajay", Marks = 90, BatchCode = "B001" };
+            return View(student);
+        }
+
+
+
+
+        public ActionResult Index1()
+        {
+            List<Student> students = new List<Student>()
+            {
+                 new Student(){ Id=1, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=2, Name="Deepak", Marks=89, BatchCode="B002"},
+                 new Student(){ Id=3, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=4, Name="Deepak", Marks=89, BatchCode="B002"},
+                 new Student(){ Id=5, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=6, Name="Deepak", Marks=89, BatchCode="B002"},
+                 new Student(){ Id=7, Name="Ajay", Marks=90, BatchCode="B001"},
+                 new Student(){ Id=8, Name="Deepak", Marks=89, BatchCode="B002"},
+                };
+             
+            return View(students);
+        }
+
+    }
+}
